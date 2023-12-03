ce00a58025ac1fdf3529eee9e3b86cd13d0dfdd0
Fran <fducekuce@gmail.com>
2023-12-03 13:11:44 +0100

    finish

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,23 @@
 const Footer = () => {
   return (
-    <footer className="container mx-auto flex flex-col items-center p-5 text-sm">
-      <p>Copyright © 2023. All rights reserved.</p>
-      <a
-        href="https://portfolio-crofrank.vercel.app/"
-        target="_blank"
-        rel="noopener"
-      >
-        Made by: <span className="text-yellow-700">Fran Branković</span>
-      </a>
+    <footer className="container mx-auto flex flex-col items-center p-2 text-sm">
+      <div className="flex flex-col items-center pb-5 sm:pr-5">
+        <p>Matija Pranjić j.d.o.o.</p>
+        <p>OIB: 18467436358</p>
+        <p>Osječka 11, Velika Kosnica, 10410 Velika Gorica</p>
+        <p>Phone: (+385) 92 102-8847</p>
+      </div>
+
+      <div className="flex flex-col items-center pb-5">
+        <p>Copyright © 2023. All rights reserved.</p>
+        <a
+          href="https://portfolio-crofrank.vercel.app/"
+          target="_blank"
+          rel="noopener"
+        >
+          Made by: <span className="text-yellow-700">Fran Branković</span>
+        </a>
+      </div>
     </footer>
   )
 }
